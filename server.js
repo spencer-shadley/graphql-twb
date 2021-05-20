a2313ce98281a708fa5c0da1e2aae747ba8e8517
Spencer Shadley <spencershadley@utexas.edu>
2021-05-19 17:00:08 -0700

    replaced version without large files

--- server.js
+++ server.js
@@ -1,34 +1,74 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql').graphqlHTTP;
-const twb = require('./simple-twb');
-const TwbType = require('./simple-schema');
-
+const jsonToSchema = require('@walmartlabs/json-to-simple-graphql-schema');
+const fs = require('fs');
+const fastXmlParser = require('fast-xml-parser');
 const {
-    GraphQLSchema,
-    GraphQLObjectType,
+    buildSchema,
 } = require('graphql');
 
 const app = express();
+const filename = 'simple-workbook.twb';
 
-const RootQueryType = new GraphQLObjectType({
-    name: 'Query',
-    description: 'Root Query',
-    fields: () => ({
-        twb: {
-            type: TwbType,
-            description: 'Entire workbook',
-            resolve: () => twb
-        }
-    })
-})
+const initialState = {
+  time: Date.now(),
+  memory: process.memoryUsage().heapUsed,
+  fileSize: getFilesizeInMB(filename)
+}
 
-const schema = new GraphQLSchema({
-    query: RootQueryType
-});
+const twbXML = readFile(filename);
+const parsedJson = parseJson(twbXML);
+const schema = deriveSchema(parsedJson);
 
-app.use('/graphql', expressGraphQL({
-    schema: schema,
-    graphiql: true
-}))
+startServer();
 
-app.listen(5000, () => console.log('server running'));
+function readFile(filename) {
+  const file = fs.readFileSync(`${__dirname}/${filename}`, 'utf8');
+  console.log('file read');
+  return file;
+}
+
+function getFilesizeInMB(filename) {
+  var stats = fs.statSync(filename);
+  var fileSizeInBytes = stats.size;
+  return fileSizeInBytes / 1024 / 1024;
+}
+
+function parseJson() {
+  const parsedJson = fastXmlParser.parse(JSON.stringify(twbXML));
+  fs.writeFileSync(__dirname + '/output/parsed.json', JSON.stringify(parsedJson));
+  console.log('json parsed');
+  return parsedJson;
+}
+
+function deriveSchema(parsedJson) {
+  const schema = jsonToSchema.jsonToSchema({ jsonInput: JSON.stringify(parsedJson) }); // duplicate parsing
+  
+  const schemaWithQuery = `
+    ${schema.value} 
+    type Query {
+      workbook: Workbook
+  }`;
+
+  fs.writeFileSync(__dirname + '/output/schema.txt', JSON.stringify(schemaWithQuery));
+  console.log('schema derived from JSON');
+  return schemaWithQuery;
+}
+
+function printMetrics() {
+  console.log((Date.now() - initialState.time) / 1000 + 's');
+  console.log((process.memoryUsage().heapUsed - initialState.memory) / 1024 /1024 + 'MB');
+}
+
+function startServer() {
+  app.use('/graphql', expressGraphQL({
+    schema: buildSchema(schema),
+    rootValue: parsedJson,
+    graphiql: true,
+  }));
+  
+  printMetrics();
+  
+  console.log('=========\n\n\n\n');
+  app.listen(5000, () => console.log('server running'));
+}
